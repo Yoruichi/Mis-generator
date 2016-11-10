e82d5e1ee54ecc5f507a87c3504dd091907b6846
yoruichi <hz-zhangcong@163.com>
2016-11-10 11:29:19 +0800

    modify model template with builder mode

--- src/main/template/${basePackagePath}/model/${className}.java
+++ src/main/template/${basePackagePath}/model/${className}.java
@@ -7,62 +7,68 @@ import me.yoruichi.mis.BasePo;
 ${importType}
 </#list>
 /**
- * 
+ *
  * ${sqlComment}
  *
  */
 public class ${className} extends BasePo {
-	
-	<#list dataModelColumns as column>
-	private ${column.javaType} ${column.fieldName};//${column.sqlComment}
-	</#list>
 
+    <#list dataModelColumns as column>
+    private ${column.javaType} ${column.fieldName};//${column.sqlComment}
+    </#list>
+    public static ${className} build() {
+        return new ${className}();
+    }
 <@generateSetterGetter/>
 
 }
 
 <#-- 定义宏，生成属性的getter和setter方法 -->
 <#macro generateSetterGetter>
-	<#list dataModelColumns as column>
-		
-		<#if column.javaType?index_of("String") = 0><#-- 如果是字符串类型的属性，在setter的时候进行trim -->
-	public void set${column.operName}(${column.javaType} ${column.fieldName}) {
-		this.${column.fieldName} = ${column.fieldName} == null ? null : ${column.fieldName}.trim();
-	}
-		<#else>
-	public void set${column.operName}(${column.javaType} ${column.fieldName}) {
-		this.${column.fieldName} = ${column.fieldName};
-	}
-		</#if>
-	public ${column.javaType} get${column.operName}() {
-		return this.${column.fieldName};
-	}
-	</#list>
+    <#list dataModelColumns as column>
+
+        <#if column.javaType?index_of("String") = 0><#-- 如果是字符串类型的属性，在setter的时候进行trim -->
+    public ${className} set${column.operName}(${column.javaType} ${column.fieldName}) {
+        this.${column.fieldName} = ${column.fieldName} == null ? null : ${column.fieldName}.trim();
+        return this;
+    }
+        <#else>
+    public ${className} set${column.operName}(${column.javaType} ${column.fieldName}) {
+        this.${column.fieldName} = ${column.fieldName};
+        return this;
+    }
+        </#if>
+    public ${column.javaType} get${column.operName}() {
+        return this.${column.fieldName};
+    }
+    </#list>
 </#macro>
 
 <#-- 定义宏，生成属性的getter方法 -->
 <#macro generateGetter>
-	<#list dataModelColumns as column>
-	public ${column.javaType} get${column.operName}() {
-		return this.${column.fieldName};
-	}
-	</#list>
+    <#list dataModelColumns as column>
+    public ${column.javaType} get${column.operName}() {
+        return this.${column.fieldName};
+    }
+    </#list>
 </#macro>
 
 <#-- 定义宏，生成属性的setter方法 -->
 <#macro generateSetter>
-	<#list dataModelColumns as column>
-		
-		<#if column.javaType?index_of("String") = 0><#-- 如果是字符串类型的属性，在setter的时候进行trim -->
-	public void set${column.operName}(${column.javaType} ${column.fieldName}) {
-		this.${column.fieldName} = ${column.fieldName} == null ? null : ${column.fieldName}.trim();
-	}
-		<#else>
-	public void set${column.operName}(${column.javaType} ${column.fieldName}) {
-		this.${column.fieldName} = ${column.fieldName};
-	}
-		</#if>
-	</#list>
+    <#list dataModelColumns as column>
+
+        <#if column.javaType?index_of("String") = 0><#-- 如果是字符串类型的属性，在setter的时候进行trim -->
+    public ${className} set${column.operName}(${column.javaType} ${column.fieldName}) {
+        this.${column.fieldName} = ${column.fieldName} == null ? null : ${column.fieldName}.trim();
+        return this;
+    }
+        <#else>
+    public ${className} set${column.operName}(${column.javaType} ${column.fieldName}) {
+        this.${column.fieldName} = ${column.fieldName};
+        return this;
+    }
+        </#if>
+    </#list>
 </#macro>
 
 
