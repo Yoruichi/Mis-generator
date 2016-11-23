cc70cf19ab1732068689a8a6bc3464ee62b4b02b
yoruichi <hz-zhangcong@163.com>
2016-11-23 19:48:14 +0800

    add controller tempate

---
+++ src/main/template/${basePackagePath}/controller/${className}Controller.java
@@ -0,0 +1,46 @@
+package ${basePackage}.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import ${basePackage}.dao.${className}Dao;
+import ${basePackage}.model.entity.${className};
+import ${basePackage}.model.page.ResponseStruct;
+import io.swagger.annotations.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/${className}")
+@Api(description = "${className}相关操作")
+public class ${className}Controller extends BaseController {
+
+    @Autowired
+    private ${className}Dao i${className}Dao;
+
+    @RequestMapping(value = "/all", method = RequestMethod.POST)
+    @ApiOperation(value = "获取全部${className}的接口", response = ${className}.class)
+    public ResponseStruct get${className}s() throws Exception {
+        return succ(i${className}Dao.selectMany(${className}.build()));
+    }
+
+    @RequestMapping(value = "/page", method = RequestMethod.POST)
+    @ApiOperation(value = "获取分页${className}的接口", response = ${className}.class)
+    public ResponseStruct get${className}sByPage(
+            @RequestParam(defaultValue = "1") int pageNum,
+            @RequestParam(defaultValue = "10") int pageSize) throws Exception {
+        if (pageNum <= 0) pageNum = 1;
+        if (pageSize <= 0) pageSize = 10;
+
+        return succ(i${className}Dao.selectMany(
+        (${className}) i${className}.build().setLimit(pageSize).setIndex((pageNum - 1) * pageSize)));
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ApiOperation(value = "添加一个${className}", response = ${className}.class)
+    public ResponseStruct add${className}(
+            @ApiParam(required = true, value = "{\n"
+                    + "\t\"property\":\"value.\"\n"
+                    + "}") @RequestBody JsonNode jsonNode
+    ) throws Exception {
+        return succ(jsonNode.toString());
+    }
+}
